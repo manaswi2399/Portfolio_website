4ea115155add0edfa0aeee903240051517f49b76
manaswi2399 <manaswirajesh.lukkad@sjsu.edu>
2024-12-18 13:23:07 -0800

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,7 +11,7 @@ export const Hero = () => {
         <p className={styles.description}>
         I'm a software developer with 3+ years of experience, specializing in Python, Java, AWS, and backend technologies. Currently pursuing my Masters in Computer Engineering, I have a keen interest in full-stack development, system design and bring expertise in both cloud computing and machine learning. Throughout my professional journey, I've navigated the intricacies of software development, honing my skills in Python, React, Java and leveraging AWS for robust backend solutions. My enthusiasm extends to exploring innovative applications in machine learning. Let's connect to discuss tech trends, potential collaborations, or insights in the ever-evolving world of software development!
         </p>
-        <a href="https://drive.google.com/file/d/1QwSi5Z4CWBFcBclOyugoXjv8hN-Lszd9/view?usp=drive_link" className={styles.contactBtn}>
+        <a href="https://drive.google.com/drive/u/0/home" className={styles.contactBtn}>
           Resume
         </a>
       </div>
